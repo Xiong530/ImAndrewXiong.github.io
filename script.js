d6c5e571828db9fac2b689854eba69c801ccabc5
Jinzoh <andrewxiong@csus.edu>
2024-06-06 03:26:31 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function scrollToElement(elementSelector, instance =0){
+    //Selects all elements that match the given selector
+
+    const elements = document.querySelectorAll(elementSelector);
+}
