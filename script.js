cf1757423973d98d832c98a7ea3c8e72e5bef5fb
Jinzoh <andrewxiong@csus.edu>
2024-06-06 04:19:14 -0700

    added id to nav-links

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function scrollToElement(elementSelector, instance =0){
+function scrollToElement(elementSelector, instance = 0){
     //Selects all elements that match the given selector
 
     const elements = document.querySelectorAll(elementSelector);
+
+    if (elements.length > instance){
+        elements[instance].scrollIntoView({ behavior: 'smooth'});
+    }
 }
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
